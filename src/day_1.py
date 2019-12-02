053b5e0456b5e0cabb6f202dfb3bab118bfea675
Keith Gough <krgough@gmail.com>
2019-12-02 16:35:24 +0000

    day 1 part 2 update

--- src/day_1.py
+++ src/day_1.py
@@ -19,31 +19,61 @@ def load_data(filename):
             data.append(int(line.strip()))
     return data
 
-def fuel_required(module_mass):
+def simple_fuel_required(module_mass):
     """ Calculate the fuel required to launch the module
 
         Fuel required to launch a given module is based on its mass.
         Specifically, to find the fuel required for a module, take its mass,
         divide by three, round down, and subtract 2.
     """
-    return math.floor(module_mass/3) - 2
+    fuel_mass = max(0, math.floor(module_mass/3) - 2)
+
+    return fuel_mass
+
+# def total_fuel_required(module_mass):
+#     """ Use a loop here and keep calling till we get zero """
+#     fuel_mass = simple_fuel_required(module_mass)
+# 
+#     extra = fuel_mass
+#     while extra > 0:
+#         extra = simple_fuel_required(extra)
+#         fuel_mass += extra
+# 
+#     return fuel_mass
+
+def total_fuel_required(module_mass):
+    """ Use regression to calculate total fuel required """
+    fuel = simple_fuel_required(module_mass)
+    if fuel <= 0:
+        return fuel
+    return fuel + total_fuel_required(fuel)
 
 def main():
     """ Main Program """
     module_mass_data = load_data(FILENAME)
 
     # Run test vectors
-    assert fuel_required(12) == 2
-    assert fuel_required(14) == 2
-    assert fuel_required(1969) == 654
-    assert fuel_required(100756) == 33583
+    assert simple_fuel_required(12) == 2
+    assert simple_fuel_required(14) == 2
+    assert simple_fuel_required(1969) == 654
+    assert simple_fuel_required(100756) == 33583
+
+    assert total_fuel_required(1969) == 966
     print ('Tests pass')
 
-    # Calculate answer
-    total_fuel = 0
+    # Calculate part1 answer
+    simple_fuel = []
     for module_mass in module_mass_data:
-        total_fuel += fuel_required(module_mass)
-    print(total_fuel)
+        simple_fuel.append(simple_fuel_required(module_mass))
+
+    print(f'Simple Fuel Total (Part 1) = {sum(simple_fuel)}')
+
+    # Calculate part2 answer
+    complex_fuel = []
+    for module_mass in module_mass_data:
+        complex_fuel.append(total_fuel_required(module_mass))
+    print(f"Total fuel required (Part 2) = {sum(complex_fuel)}")
+
 
 if __name__ == "__main__":
     main()
